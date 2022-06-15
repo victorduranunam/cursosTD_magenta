aee14017bbcb0ea3e58c0831e244544070ac684b
UstehKenny <carosiim.sdco@gmail.com>
2022-06-15 14:20:57 -0500

    Controllers
    Todos los modelos tienen controllers y ya se hizo la prueba con la BD.
    Falta agregar un privilegio al usuario de magesticd para poder hacer consultas.

---
+++ app/Http/Controllers/DivisionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Division;
+class DivisionController extends Controller
+{
+    //Prueba -> Funciona!
+    public function index()
+    {
+        $users = Division::all()->first();
+        return view("welcome")
+            ->with("users",$users);
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Prueba -> Funciona!
+//Route::get('division', "App\Http\Controllers\DivisionController@index")->name("division.consulta");
