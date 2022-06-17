73c48a14f235a83871bfea3ff363401587a725b3
UstehKenny <carosiim.sdco@gmail.com>
2022-06-17 12:15:36 -0500

    Página welcome
    Primera pág lista, sólo falta la autenticación

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,26 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href={!! asset('bootstrap/css/bootstrap.min.css') !!}>
   <link rel="stylesheet" href={!! asset('css/app.css') !!}>
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
   <title>MagestiCD | Centro de Docencia</title>
 </head>
-<body>
-  <div>
-    <a href="" class="prueba">Hola</a>
+<body class="home">
+  
+<div class="flex-center position-ref full-height">
+           
+  <div class="top-right links">             
+    <a href="">Hogar</a>             
+    <a href="">Ingresar</a>              
   </div>
+          
+  <div class="content">
+    <div class="title m-b-md">
+      MAGESTICD
+    </div>               
+  </div>
+
+</div>
 
   <script src={!! asset('bootstrap/js/bootstrap.min.js') !!} ></script>
 </body>
