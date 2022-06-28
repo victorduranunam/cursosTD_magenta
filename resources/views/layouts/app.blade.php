91bc1ec232b418556de6e8ebbbb80fc8e4829193
UstehKenny <carosiim.sdco@gmail.com>
2022-06-28 11:09:30 -0500

    Side Menu
    Ready header, footer and menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,8 @@
         
         <!-- User Dashboard -->
         <div class="dropdown">
-          <a href="#" class="dropdown-toggle usr-dashboard" data-bs-toggle="dropdown">UserName &nbsp;<i class="bi bi-person"></i></a>
-            <div class="dropdown-menu dropleft dropdown-menu-end">
+          <a href="#" class="dropdown-toggle usr-dashboard" data-bs-toggle="dropdown">UserName &nbsp;<i class="bi bi-person-circle"></i></a>
+            <div class="dropdown-menu dropdown-menu-end">
                 <a href="#" class="dropdown-item">Ver Usuarios</a>
                 <a href="#" class="dropdown-item">Cerrar Sesión</a>
             </div>
@@ -37,18 +37,97 @@
     <!-- Side menu -->
     <div class="container-fluid">
       <div class="row">
-        <div class="col-10 col-lg-3 side-menu">....</div>
-        <div class="col-lg-9 main-info">....</div>
+        <!-- Side menu Content -->
+        <div class="wrap col-10 col-lg-3 side-menu">
+          <aside id="side-menu" class="aside" role="navigation">
+              <ul class="nav nav-list accordion">
+                  <li class="nav-header">
+                      <div class="link"><i class="bi bi-send"></i> Diplomados<i class="bi bi-chevron-down"></i></div>
+                      <ul class="submenu">
+                        <li><a href="">Ver Diplomados</a></li>
+                        <li><a href="">Alta de diplomado</a></li>
+                        <li><a href="">Ver Módulos Programados</a></li>
+                        <li><a href="">Ver Catálogo de Módulos</a></li>
+                        <li><a href="">Alta de módulo</a></li>
+
+                      </ul>
+                  </li>
+
+                  <li class="nav-header">
+                      <div class="link"><i class="bi bi-journals"></i>Cursos<i class="bi bi-chevron-down"></i></div>
+                      <ul class="submenu">
+                          <li><a href="">Cursos programados</a></li>
+                          <li><a href="">Catálogo de cursos</a></li>
+                          <li><a href="">Alta Catálogo</a></li>
+                      </ul>
+                  </li>
+
+                  <li class="nav-header">
+                      <div class="link"><i class="bi bi-person-lines-fill"></i>Profesores<i class="bi bi-chevron-down"></i></div>
+                      <ul class="submenu">
+                          <li><a href="">Alta Profesor</a></li>
+                          <li><a href="">Consulta de profesores</a></li>
+                          <li><a href="">Categoría y Nivel</a></li>
+                      </ul>
+                  </li>
+
+                  <li class="nav-header">
+                      <div class="link"><i class="bi bi-building"></i>Salones<i class="bi bi-chevron-down"></i></div>
+                      <ul class="submenu">
+                          <li><a href="">Consulta Salones</a></li>
+                          <li><a href="">Alta de Salón</a></li>
+                      </ul>
+                  </li>
+                  <li class="nav-header">
+                      <div class="link"><i class="bi bi-mortarboard"></i>Carreras<i class="bi bi-chevron-down"></i></div>
+                      <ul class="submenu">
+                          <li><a href="">Consulta Carreras</a></li>
+                          <li><a href="">Alta de Carreras</a></li>
+                          <!--<li><a href="#">Baja de Salon</a></li>-->
+                      </ul>
+                  </li>
+                  <li class="nav-header">
+                      <div class="link"><i class="bi bi-geo-alt"></i>Coordinaciones<i class="bi bi-chevron-down"></i></div>
+                      <ul class="submenu">
+                          <li><a href="">Alta de Coordinación</a></li>
+                          <li><a href="">Consulta de coordinaciones</a></li>
+                          <li><a href="">Coordinación del Centro de Docencia</a></li>
+                          <li><a href="">Secretaría de apoyo</a></li>
+                          <li><a href="">Dirección</a></li>
+
+                      </ul>
+                  </li>
+
+                  <li class="nav-header">
+                      <div class="link"><i class="bi bi-briefcase"></i>Divisiones<i class="bi bi-chevron-down"></i></div>
+                      <ul class="submenu">
+                          <li><a href="">Alta de División</a></li>
+                          <li><a href="">Consulta de Divisiones</a></li>
+                      </ul>
+                  </li>
+                  <li class="nav-header">
+                      <div class="link"><i class="bi bi-journal-check"></i>Evaluaciones<i class="bi bi-chevron-down"></i></div>
+                      <ul class="submenu">
+                          <li><a href="">Área 1</a></li>
+                          <li><a href="">Área 2</a></li>
+                      </ul>
+                  </li>
+              </ul>
+          </aside>
+        </div>
+        <!-- Main Info -->
+        <div class="col-lg-9 main-info">
+          @yield('content')
+        </div>
       </div>
     </div>
 
-    <!-- @yield('content') -->
-
-    <footer class="text-center footer footer-link">
+    
+    <!-- <footer class="text-center footer footer-link"> -->
       <!-- Grid container -->
-      <div class="container">
+      <!-- <div class="container"> -->
         <!-- Section: Links -->
-        <section class="mt-5">
+        <!-- <section class="mt-5">
 
           <div class="row text-center d-flex justify-content-center pt-5">
             <div class="col-md-2">
@@ -83,9 +162,11 @@
           </div>
         </section>
       </div>
-    </footer>
+    </footer> -->
   </div>
-
+  <script src={!! asset('bootstrap/js/jquery.js') !!}></script>
+  <script src={!! asset('bootstrap/js/admin.js') !!}></script>
+  <script src={!! asset ('/dist/jquery.fancybox.min.js') !!}></script>
   <script src={!! asset('bootstrap/js/bootstrap.min.js') !!}></script>
   <script src={!! asset('bootstrap/js/bootstrap.bundle.js') !!}></script>
 </body>
