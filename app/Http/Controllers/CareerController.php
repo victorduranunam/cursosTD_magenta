b3d8472a2ed48b75bdd16b08f7521ec45f1beff0
Mauricio <47121742+MauRamos334455@users.noreply.github.com>
2022-07-21 14:24:05 -0500

    Final career module

--- app/Http/Controllers/CareerController.php
+++ app/Http/Controllers/CareerController.php
@@ -8,105 +8,115 @@ use Illuminate\Support\Facades\DB;
 
 class CareerController extends Controller
 {
-    public function index()
-    {
-        try {
-            $careers = Career::all();
-            return view("pages.view-careers")
-                ->with("careers",$careers);
-            
-        } catch (\Illuminate\Database\QueryException $th) {
-            return redirect()
-                ->route('home')
-                ->with('danger', 'Problema con la base de datos.');
-        }
+  public function index()
+  {
+    try {
+      $careers = Career::all();
+      return view("pages.view-careers")
+        ->with("careers", $careers);
+    } catch (\Illuminate\Database\QueryException $th) {
+      return redirect()
+        ->route('home')
+        ->with('danger', 'Problema con la base de datos.');
     }
+  }
 
-    public function create(){
-        return view ("pages.create-career");
+  public function create()
+  {
+    return view("pages.create-career");
+  }
+
+  public function store(Request $req)
+  {
+    try {
+      $career = new Career();
+      $career->career_id = DB::select("select nextval('career_seq')")[0]->nextval;
+      $career->name = $req->name;
+      $career->key = $req->key;
+      $career->save();
+
+      return redirect()
+        ->route('view.careers')
+        ->with('success', 'Carrera creada correctamente');
+    } catch (\Illuminate\Database\QueryException $th) {
+      if ($th->getCode() == 7)
+        return redirect()
+          ->route('home')
+          ->with('danger', 'Problema con la base de datos.');
+      elseif ($th->getCode() == 23505)
+        return redirect()
+          ->back()
+          ->with('career', $career)
+          ->with('warning', 'Error al almacenar, recuerde que la clave debe ser única para cada carrera.');
+      else
+        return redirect()
+          ->back()
+          ->with('warning', 'Error al almacenar, verifique sus datos.')
+          ->withInput();
     }
+  }
 
-    public function store(Request $req){
-        try {
-            $career = new Career();
-            $career->career_id = DB::select("select nextval('career_seq')")[0]->nextval;
-            $career->name = $req->name;
-            $career->key = $req->key;
-            $career->save();
-
-            return redirect()
-              ->route('view.careers')
-              ->with('success', 'Carrera creada correctamente');
-
-        } catch (\Illuminate\Database\QueryException $th) {
-            if($th->getCode() == 7)
-                return redirect()
-                  ->route('home')
-                  ->with('danger', 'Problema con la base de datos.');
-            else
-                return redirect()
-                  ->back()
-                  ->with('warning', 'Error al almacenar, verifique sus datos.')
-                  ->withInput();
-        }
+  public function edit($career_id)
+  {
+    try {
+      $career = Career::findOrFail($career_id);
+      return view("pages.update-career")
+        ->with("career", $career);
+    } catch (\Illuminate\Database\QueryException $th) {
+      return redirect()
+        ->route('view.careers')
+        ->with('danger', 'Problema con la base de datos.');
     }
+  }
 
-    public function edit($career_id){
-        try {
-            $career = Career::findOrFail($career_id);
-            return view("pages.update-career")
-              ->with("career",$career);
-        } catch (\Illuminate\Database\QueryException $th) {
-            return redirect()
-              ->route('view.careers')
-              ->with('danger','Problema con la base de datos.');
-        }
+  public function update(Request $req, $career_id)
+  {
+    try {
+      $career = career::findOrFail($career_id);
+      $career->name = $req->name;
+      $career->key = $req->key;
+      $career->save();
+
+      return redirect()
+        ->route('edit.career', $career->career_id)
+        ->with('success', 'Cambios realizados.');
+    } catch (\Illuminate\Database\QueryException $th) {
+      if ($th->getCode() == 7)
+        return redirect()
+          ->route('home')
+          ->with('danger', 'Problema con la base de datos.');
+      elseif ($th->getCode() == 23505)
+        return redirect()
+          ->back()
+          ->with('career', $career)
+          ->with('warning', 'Error al almacenar, recuerde que la clave debe ser única para cada carrera.');
+      else
+        return redirect()
+          ->back()
+          ->with('career', $career)
+          ->with('warning', 'Error al almacenar, verifique sus datos.');
     }
+  }
 
-    public function update(Request $req, $career_id){
-        try {
-            $career = career::findOrFail($career_id);
-            $career->name = $req->name;
-            $career->key = $req->key;
-            $career->save();
+  public function delete($career_id)
+  {
+    try {
+      $career = Career::findOrFail($career_id);
+      $career->delete();
 
-            return redirect()
-              ->route('edit.career',$career->career_id)
-              ->with('success', 'Cambios realizados.');
-        } catch (\Illuminate\Database\QueryException $th) {
-            if($th->getCode() == 7)
-                return redirect()
-                  ->route('home')
-                  ->with('danger', 'Problema con la base de datos.');
-            else
-                return redirect()
-                  ->back()
-                  ->with('career',$career)
-                  ->with('warning', 'Error al almacenar, verifique sus datos.');
-        }
+      return redirect()
+        ->route('view.careers')
+        ->with('success', 'Eliminado correctamente.');
+    } catch (\Illuminate\Database\QueryException $th) {
+
+      if ($th->getCode() == 7)
+        return redirect()
+          ->route('home')
+          ->with('danger', 'Problema con la base de datos.');
+      else
+        return redirect()
+          ->back()
+          ->with('warning', 'Error al eliminar el salón.');
     }
-
-    public function delete($career_id){
-        try {
-          $career = Career::findOrFail($career_id);
-          $career->delete();
-          
-          return redirect()
-            ->route('view.careers')
-            ->with('success', 'Eliminado correctamente.');
-    
-        } catch (\Illuminate\Database\QueryException $th) {
-          
-          if($th->getCode() == 7)
-            return redirect()
-              ->route('home')
-              ->with('danger', 'Problema con la base de datos.');
-          else
-            return redirect()
-              ->back()
-              ->with('warning', 'Error al eliminar la coordinación.');
-        }
-      }
-
-
+  }
 }
--- resources/views/pages/view-careers.blade.php
+++ resources/views/pages/view-careers.blade.php
@@ -30,7 +30,9 @@
                           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                          <p>¿Está seguro de eliminar la carrera {!! $career->name !!}? 
+                          <p>¿Está seguro de eliminar la carrera {!! $career->name !!}?
+                            Esto borrará los registros que existan entre profesores
+                            con ella.
                           </p>
                         </div>
                         <div class="modal-footer">
